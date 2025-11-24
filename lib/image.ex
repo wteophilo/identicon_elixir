14f2f28da1872e804ee802140d6f730e8e8cc2be
Willian Teophilo <wteophilo@gmail.com>
2025-11-24 10:27:34 -0300

    feat: add image struct

---
+++ lib/image.ex
@@ -0,0 +1,7 @@
+defmodule Identicon.Image do
+  @moduledoc """
+    Provides data struct for Images
+  """
+
+  defstruct hex: nil
+end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -5,13 +5,13 @@ defmodule IdenticonTest do
   describe "hash_input/1" do
     test "returns a known MD5 hash for a specific input" do
       input = "elixir"
-      expected_output = [116, 181, 101, 134, 90, 25, 44, 200, 105, 60, 83, 13, 72, 235, 56, 58]
+      expected_output = %Identicon.Image{hex: [116, 181, 101, 134, 90, 25, 44, 200, 105, 60, 83, 13, 72, 235, 56, 58]}
       assert Identicon.hash_input(input) == expected_output
     end
 
     test "returns a list of length 16 (MD5 standard)" do
       result = Identicon.hash_input("any random string")
-      assert length(result) == 16
+      assert length(result.hex) == 16
     end
 
     test "produces different hashes for different inputs" do
