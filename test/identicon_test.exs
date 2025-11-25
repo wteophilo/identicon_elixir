6ef62ae3540a42854d2167b997f31e2571946aaa
Willian Teophilo <wteophilo@gmail.com>
2025-11-25 10:36:06 -0300

    feat: create function to chunk and mirror an array

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -26,7 +26,7 @@ defmodule IdenticonTest do
     end
   end
 
-  describe "pick_color" do
+  describe "pick_color/1" do
     test "return initial three number of array" do
       input = %Identicon.Image{
         hex: [116, 181, 101, 134, 90, 25, 44, 200],
@@ -39,6 +39,30 @@ defmodule IdenticonTest do
       }
 
       assert Identicon.pick_color(input) == expected_output
+    end
+  end
+
+  describe "mirror_row/1" do
+    test "returns a line with the first two items mirrored" do
+      input = [116, 181, 101]
+      expected_output = [116, 181, 101, 181, 116]
+      assert Identicon.mirror_row(input) == expected_output
+    end
+  end
+
+  describe "build_grid/1" do
+    test "should return a new list chunked and mirrored" do
+      input = %Identicon.Image{
+        hex: [116, 181, 101, 134, 90, 25, 44, 200 , 100],
+        color: {116, 181, 101}
+      }
+
+      expected_output = %Identicon.Image{
+        hex: [[116, 181, 101, 181, 116], [134, 90, 25, 90, 134], [44, 200, 100, 200, 44]],
+        color: {116, 181, 101}
+      }
+
+      assert Identicon.build_grid(input) == expected_output.hex
     end
   end
 end
